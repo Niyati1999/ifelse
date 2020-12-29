19778a49e89436a76667db582fbffd9a310e33a9
niyati salot <170120107148@git.org.in>
2020-12-29 11:47:53 +0530

    Add project files.

---
+++ ifelse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter three Numbers : ");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            //string c = Console.ReadLine();
+            int x = Convert.ToInt32(a);
+            int y = Convert.ToInt32(b);
+            int z = Convert.ToInt32(Console.ReadLine());
+            if (x > y && x>z)
+            {
+                Console.WriteLine("Max is " + x);
+            }
+            else if(y>z)
+            {
+                Console.WriteLine("max is " + y);
+            }
+            else
+            {
+                Console.WriteLine("max is " + z);
+            }
+        }
+    }
+}
